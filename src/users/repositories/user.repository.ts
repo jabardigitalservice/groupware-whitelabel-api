cdd2516d954cee9dc8addbebb6aacd8c877530b9
zidanesc <zidankren02@gmail.com>
2022-03-21 17:14:03 +0700

    feat(user): add endpoint get users

--- src/users/repositories/user.repository.ts
+++ src/users/repositories/user.repository.ts
@@ -1,4 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { GetUsersFilterDto } from '../dto/get-users-filter.dto';
 import { User } from '../entities/user.entity';
 
 @EntityRepository(User)
@@ -8,5 +9,23 @@ export class UserRepository extends Repository<User> {
       .relation(User, 'roles')
       .of(userId)
       .add(roleId);
+  }
+
+  getUsers(getUsersFilterDto: GetUsersFilterDto): Promise<User[]> {
+    const { name } = getUsersFilterDto;
+
+    const query = this.createQueryBuilder('user')
+      .select(['user.id', 'user.name', 'userProfile.avatar AS avatar'])
+      .innerJoin('user.userProfile', 'userProfile')
+      .where('user.deleted_at IS NULL')
+      .orderBy('user.name', 'ASC');
+
+    if (name) {
+      query.andWhere('LOWER(user.name) LIKE LOWER(:name)', {
+        name: `%${name}%`,
+      });
+    }
+
+    return query.getRawMany();
   }
 }
