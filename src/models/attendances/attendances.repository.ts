34ab2f6df2552d900b049a61c9ca4bd6c0058ee0
Fajar Hikmal <fajarhikmal214@gmail.com>
2022-03-24 08:09:12 +0700

    refactor(attendance): Add try catch on attendance repository when quering

--- src/models/attendances/attendances.repository.ts
+++ src/models/attendances/attendances.repository.ts
@@ -1,37 +1,50 @@
 import { User } from '../../users/entities/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { Attendance } from './entities/attendance.entity';
+import { InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(Attendance)
 export class AttendancesRepository extends Repository<Attendance> {
   async findByUserAndToday(user: User, today: string): Promise<Attendance> {
-    const attendance = await this.createQueryBuilder('attendances')
-      .where('attendances.user_id = :user_id', { user_id: user.id })
-      .andWhere('DATE(attendances.start_date) = :today', { today })
-      .orderBy('attendances.start_date', 'ASC')
-      .getOne();
+    try {
+      const attendance = await this.createQueryBuilder('attendances')
+        .where('attendances.user_id = :user_id', { user_id: user.id })
+        .andWhere('DATE(attendances.start_date) = :today', { today })
+        .orderBy('attendances.start_date', 'ASC')
+        .getOne();
 
-    return attendance;
+      return attendance;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   async isCheckedIn(user: User, today: string): Promise<Attendance> {
-    const isCheckedIn = await this.createQueryBuilder('attendances')
-      .where('attendances.user_id = :user_id', { user_id: user.id })
-      .andWhere('DATE(attendances.start_date) = :today', { today })
-      .orderBy('attendances.start_date', 'ASC')
-      .getOne();
+    try {
+      const isCheckedIn = await this.createQueryBuilder('attendances')
+        .where('attendances.user_id = :user_id', { user_id: user.id })
+        .andWhere('DATE(attendances.start_date) = :today', { today })
+        .orderBy('attendances.start_date', 'ASC')
+        .getOne();
 
-    return isCheckedIn;
+      return isCheckedIn;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   async isCheckedOut(user: User, today: string): Promise<Attendance> {
-    const isCheckedOut = await this.createQueryBuilder('attendances')
-      .where('attendances.user_id = :user_id', { user_id: user.id })
-      .andWhere('DATE(attendances.start_date) = :today', { today })
-      .andWhere('attendances.end_date IS NOT NULL')
-      .orderBy('attendances.start_date', 'ASC')
-      .getOne();
+    try {
+      const isCheckedOut = await this.createQueryBuilder('attendances')
+        .where('attendances.user_id = :user_id', { user_id: user.id })
+        .andWhere('DATE(attendances.start_date) = :today', { today })
+        .andWhere('attendances.end_date IS NOT NULL')
+        .orderBy('attendances.start_date', 'ASC')
+        .getOne();
 
-    return isCheckedOut;
+      return isCheckedOut;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 }
