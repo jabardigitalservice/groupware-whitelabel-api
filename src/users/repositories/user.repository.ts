7e93a2fcaacdc9af17eabacb90917ce838e2fdce
zidanesc <zidankren02@gmail.com>
2022-03-24 13:01:31 +0700

    refactor(user): add try-catch in the query process

--- src/users/repositories/user.repository.ts
+++ src/users/repositories/user.repository.ts
@@ -1,3 +1,4 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { GetUsersFilterDto } from '../dto/get-users-filter.dto';
 import { User } from '../entities/user.entity';
@@ -14,22 +15,26 @@ export class UserRepository extends Repository<User> {
   getUsers(getUsersFilterDto: GetUsersFilterDto): Promise<User[]> {
     const { name } = getUsersFilterDto;
 
-    const query = this.createQueryBuilder('user')
-      .select([
-        'user.id AS id',
-        'user.name AS name',
-        'userProfile.avatar AS avatar',
-      ])
-      .innerJoin('user.userPsrofile', 'userProfile')
-      .where('user.deleted_at IS NULL')
-      .orderBy('user.name', 'ASC');
+    try {
+      const query = this.createQueryBuilder('user')
+        .select([
+          'user.id AS id',
+          'user.name AS name',
+          'userProfile.avatar AS avatar',
+        ])
+        .innerJoin('user.userProfile', 'userProfile')
+        .where('user.deleted_at IS NULL')
+        .orderBy('user.name', 'ASC');
 
-    if (name) {
-      query.andWhere('LOWER(user.name) LIKE LOWER(:name)', {
-        name: `%${name}%`,
-      });
+      if (name) {
+        query.andWhere('LOWER(user.name) LIKE LOWER(:name)', {
+          name: `%${name}%`,
+        });
+      }
+
+      return query.getRawMany();
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-
-    return query.getRawMany();
   }
 }
