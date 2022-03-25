1ac52d55bf5ac406e48e64bacf9247aa3997161c
zidanesc <zidankren02@gmail.com>
2022-03-25 14:36:01 +0700

    refactor(Sentry): use moment instead Date object

--- src/common/exceptions/http-error.filter.ts
+++ src/common/exceptions/http-error.filter.ts
@@ -14,6 +14,7 @@ import {
   HttpExceptionResponse,
 } from '../interfaces/http-exception-response.interface';
 import Sentry from '../../providers/logging/sentry/sentry.module';
+import * as moment from 'moment';
 
 @Catch()
 export class HttpErrorFilter implements ExceptionFilter {
@@ -83,7 +84,7 @@ export class HttpErrorFilter implements ExceptionFilter {
     error: errorMessage,
     path: request.url,
     method: request.method,
-    timeStamp: new Date().toString(),
+    timeStamp: moment().format('YYYY-MM-DD HH:mm:ss'),
   });
 
   private getProductionErrorResponse = (
