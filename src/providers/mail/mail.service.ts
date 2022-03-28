4b8d387d144a44f391af17ec82ec2d78c1bd4aa7
zidanesc <zidankren02@gmail.com>
2022-03-28 14:20:22 +0700

    refactor(mail): delete unused file

--- src/providers/mail/mail.service.ts
+++ src/providers/mail/mail.service.ts
@@ -5,29 +5,4 @@ import lang from 'src/common/language/configuration';
 import { AppConfigService } from 'src/config/app/config.service';
 
 @Injectable()
-export class MailService {
-  constructor(
-    private mailerService: MailerService,
-    private appConfigService: AppConfigService,
-  ) {}
-
-  async sendForgotPassword(user: User, token: string) {
-    const url = `${this.appConfigService.forgotPasswordUrl}?token=${token}`;
-
-    try {
-      const mail = await this.mailerService.sendMail({
-        to: user.email,
-        subject: lang.__('mail.forgotPassword.subject'),
-        template: './dist/views/mails/forgot-password',
-        context: {
-          name: user.name,
-          url,
-        },
-      });
-
-      return mail;
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
-    }
-  }
-}
+export class MailService {}
