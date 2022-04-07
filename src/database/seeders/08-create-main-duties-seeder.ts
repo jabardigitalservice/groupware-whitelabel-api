7a2764f126d6a27eeb97cdd531ff7b98e37c0c32
zidanesc <zidankren02@gmail.com>
2022-04-07 09:10:58 +0700

    refactor(MainDuty): change insert schema in main duties seeder

--- src/database/seeders/08-create-main-duties-seeder.ts
+++ src/database/seeders/08-create-main-duties-seeder.ts
@@ -1,23 +1,29 @@
-import { Factory, Seeder } from 'typeorm-seeding';
+import { Seeder } from 'typeorm-seeding';
 import { createQueryBuilder } from 'typeorm';
 import { JobTitle } from '../../models/job-titles/entities/job-titles.entity';
 import { MainDuty } from '../../models/main-duties/entities/main-duties.entity';
 import { faker } from '@faker-js/faker';
 
 export default class CreateMainDuties implements Seeder {
-  public async run(factory: Factory): Promise<any> {
+  public async run(): Promise<any> {
     const jobTitles = await createQueryBuilder()
       .from(JobTitle, 'job_titles')
       .execute();
 
     for (const jobTitle of jobTitles) {
+      console.log(jobTitle);
+
       for (let i = 0; i < Math.floor(Math.random() * 3) + 1; i++) {
-        await factory(MainDuty)().create({
-          jobTitleId: jobTitle.id,
-          name: faker.company.catchPhraseAdjective(),
-          sequence: i + 1,
-          occupationTarget: Math.floor(Math.random() * 7) + 1,
-        });
+        await createQueryBuilder()
+          .insert()
+          .into(MainDuty)
+          .values({
+            jobTitleId: jobTitle.id,
+            name: faker.company.catchPhraseAdjective(),
+            sequence: i + 1,
+            occupationTarget: Math.floor(Math.random() * 7) + 1,
+          })
+          .execute();
       }
     }
   }
