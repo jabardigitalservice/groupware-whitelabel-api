390d8c82fe3e663256f5b88d795cf65cd4be5309
Your Name <you@example.com>
2022-04-12 10:09:50 +0700

    chore: Modify useSSL value on minio config be dynamic by env

--- src/providers/storage/minio/minio.module.ts
+++ src/providers/storage/minio/minio.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MinioModule } from 'nestjs-minio-client';
+import { AppConfigModule } from 'src/config/app/config.module';
+import { AppConfigService } from 'src/config/app/config.service';
 import { MinioConfigModule } from 'src/config/storage/minio-client/config.module';
 import { MinioConfigService } from 'src/config/storage/minio-client/config.service';
 import { MinioProviderService } from './minio.service';
 
 @Module({
   imports: [
+    AppConfigModule,
     MinioConfigModule,
     MinioModule.registerAsync({
-      imports: [MinioConfigModule],
-      inject: [MinioConfigService],
-      useFactory: async (minioConfigService: MinioConfigService) => ({
+      imports: [AppConfigModule, MinioConfigModule],
+      inject: [AppConfigService, MinioConfigService],
+      useFactory: async (
+        appConfigService: AppConfigService,
+        minioConfigService: MinioConfigService,
+      ) => ({
         endPoint: minioConfigService.host,
         port: minioConfigService.port,
-        useSSL: false,
+        useSSL: Boolean(appConfigService.env === 'production'),
         accessKey: minioConfigService.accessKey,
         secretKey: minioConfigService.secretKey,
       }),
