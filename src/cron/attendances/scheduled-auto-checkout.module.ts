00bf88e3c8fee446e18640e47db0f236b35edc55
Your Name <you@example.com>
2022-04-13 13:10:37 +0700

    refactor(attendances): Add validation if isCheckedIn is normal checkin or daysOff

--- src/cron/attendances/scheduled-auto-checkout.module.ts
+++ src/cron/attendances/scheduled-auto-checkout.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppConfigModule } from '../../config/app/config.module';
 import { AppConfigService } from '../../config/app/config.service';
 import { AttendancesRepository } from '../../models/attendances/attendances.repository';
+import { DaysOffRepository } from '../../models/days-off/days-off.repository';
 import { AttendancesService } from '../../models/attendances/attendances.service';
 import { ScheduledAttendancesAutoCheckoutService } from './scheduled-auto-checkout.service';
 
@@ -12,7 +13,7 @@ import { ScheduledAttendancesAutoCheckoutService } from './scheduled-auto-checko
   imports: [
     ConfigModule,
     AppConfigModule,
-    TypeOrmModule.forFeature([AttendancesRepository]),
+    TypeOrmModule.forFeature([AttendancesRepository, DaysOffRepository]),
     ScheduleModule.forRoot(),
   ],
   providers: [
