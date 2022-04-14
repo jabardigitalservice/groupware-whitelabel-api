6f794d595e07208259ca88e5ac37a55fb548ea08
zidanesc <zidankren02@gmail.com>
2022-04-14 13:12:27 +0700

    refactor(Logbook): change validation date task

--- src/models/logbooks/dto/create-logbook.dto.ts
+++ src/models/logbooks/dto/create-logbook.dto.ts
@@ -25,12 +25,9 @@ export class CreateLogbookDto {
   main_duty_id: string;
 
   @IsNotEmpty()
-  @Matches(
-    /([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])(T)(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9]).([0-9]){1,}(Z$)/,
-    {
-      message: lang.__('attendances.date.error'),
-    },
-  )
+  @Matches(/([0-9]{4})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/, {
+    message: lang.__('attendances.date.error'),
+  })
   date_task: Date;
 
   @IsNotEmpty()
