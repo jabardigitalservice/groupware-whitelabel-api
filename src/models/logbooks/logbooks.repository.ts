19c4c2a51772513420911c167c743d35552bb32a
zidanesc <zidankren02@gmail.com>
2022-04-14 13:26:10 +0700

    feat(Logbook): add meta object to response pagination

--- src/models/logbooks/logbooks.repository.ts
+++ src/models/logbooks/logbooks.repository.ts
@@ -21,7 +21,7 @@ export class LogbooksRepository extends Repository<Logbook> {
   async getLogbookByUserId(
     user: User,
     getLogbooksFilterDto: GetLogbooksFilterDto,
-  ): Promise<Logbook[]> {
+  ): Promise<any> {
     try {
       const {
         startDate = moment().format('YYYY-MM-DD 00:00:00'),
@@ -33,17 +33,32 @@ export class LogbooksRepository extends Repository<Logbook> {
       const skip = (page - 1) * limit;
       const userId = user.id;
 
-      const logbook = this.createQueryBuilder('logbook');
-      logbook.where('logbook.userId = :userId', { userId });
-      logbook.andWhere('logbook.deletedAt IS NULL');
-      logbook.andWhere('(logbook.dateTask BETWEEN :startDate AND :endDate)', {
-        startDate,
-        endDate,
-      });
-      logbook.take(limit);
-      logbook.skip(skip);
+      const queryLogbooks = this.createQueryBuilder('logbook');
+      queryLogbooks.where('logbook.userId = :userId', { userId });
+      queryLogbooks.andWhere('logbook.deletedAt IS NULL');
+      queryLogbooks.andWhere(
+        '(logbook.dateTask BETWEEN :startDate AND :endDate)',
+        {
+          startDate,
+          endDate,
+        },
+      );
 
-      return await logbook.getRawMany();
+      const count: number = await queryLogbooks.getCount();
+
+      queryLogbooks.take(limit);
+      queryLogbooks.skip(skip);
+      const logbooks = await queryLogbooks.getRawMany();
+
+      return {
+        logbooks,
+        meta: {
+          total: count,
+          page: +page,
+          lastPage: Math.ceil(count / limit),
+          firstPage: 1,
+        },
+      };
     } catch (error) {
       throw new InternalServerErrorException(error.message);
     }
